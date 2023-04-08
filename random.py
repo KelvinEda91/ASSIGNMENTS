2ffd72acf23b9cc664518946e3a9576c551a318a
Kelvin Edafe <jerrygreg91@gmail.com>
2023-04-08 01:09:21 +0100

    second commit

---
+++ random.py
@@ -0,0 +1,8 @@
+# #range = start - stop
+# #print(range(10)) #0-9, stop
+# for x is range(2, 20, 3):   #start = 2 and stop 10 step
+#     print(x) #0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+
+import random
+
+print(random.randrange(2, 20, 2))
