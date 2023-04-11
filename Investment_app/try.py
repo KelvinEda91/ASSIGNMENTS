20328865557fbe3188081143efda956865bd8155
Kelvin Edafe <jerrygreg91@gmail.com>
2023-04-11 13:49:00 +0100

    investapp

--- Investment_app/try.py
+++ Investment_app/try.py
@@ -7,8 +7,8 @@ def investment():
         return
     #this try: function above is a special function that prevent the user from inputing any other data type that is not Integer for the amount.
 
-    welcome_bonus = 0.5  # assuming welcome bonus is 50% expressed as a decimal
-    bonus_amount = amount * welcome_bonus
+
+    bonus_amount = amount / 100 * 50
     total_amount = amount + bonus_amount
     
     print(f"Total balance: N{total_amount}")
@@ -16,24 +16,25 @@ def investment():
     print(f"Thank you, {name}! Your initial investment of N{amount} has earned you a welcome bonus of N{bonus_amount} bringing your total investment balance to N{total_amount} ")
     
     Bal = total_amount
-    VAT = 0.07  # assuming VAT is 7% expressed as a decimal
 
     try:
-        withdrew = int(input("Input amount to withdraw: N"))
+        withdraw = int(input("Input amount to withdraw: N"))
     except ValueError:
         print("Invalid input. Please enter a valid figure amount.")
         return
+    
+    VAT = withdraw / 100 * 7
 
-    if withdrew > Bal:
-        print(" Amount Exceed Your Balance.")
-        return
 
-    if Bal and (withdrew + (withdrew * VAT)) > Bal:
+    if Bal and withdraw + VAT > Bal:
         print("You cannot withdraw up to that amount 7% VAT inclusive.")
     else:
-        Bal -= withdrew
-        Bal -= (withdrew * VAT)
+        Bal -= withdraw
+        Bal -= (withdraw - VAT)
         print(f"Successful withdrawal. 7% VAT has been deducted from your wallet balance, current balance now: N{Bal} ")
+        
+        print("hello!! we are always here to serve you and to give you the best service. please don't forget to reach out to us whenever you have an issue")
+
         return Bal
 
 investment()
